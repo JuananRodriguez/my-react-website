d72db347f6da72f3a5f6bb3dec021dc0c8cfb10c
juanan <juanan@secmotic.com>
2019-03-01 13:34:41 +0100

    añadida sobra al menu

--- src/elements/Drawer/index.js
+++ src/elements/Drawer/index.js
@@ -10,7 +10,9 @@ const Drawer = Styled('div')`
   z-index: 5000;
   background: white;
   transition: transform 176ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
-  transform: translateX( ${p=>!p.open ? p.fullWidth ? '-100%' : -(p.width+2) + 'px' : '0px'} )
+  transform: translateX( ${p=>!p.open ? p.fullWidth ? '-100%' : -(p.width+2) + 'px' : '0px'} );
+  box-shadow: 0px 8px 10px -5px rgba(0,0,0,0.2), 0px 16px 24px 2px rgba(0,0,0,0.14), 0px 6px 30px 5px rgba(0,0,0,0.12);
+
 `;
 
 Drawer.defaultProps = {
